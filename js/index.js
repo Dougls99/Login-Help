6ea1fbd9b93f6bea69d3186a742dd414b6f92fa2
Douglas <dougls99@outlook.com>
2021-08-28 00:05:52 -0300

    pag de cadastro

--- js/index.js
+++ js/index.js
@@ -14,7 +14,41 @@ function showPassword() {
     }
 };
 
-document.getElementById('btn-login').addEventListener('click', function(e) {
-    e.preventDefault();
-    alert('Algo Está Errado !');
-});
+/*Esconder senha 1*/ 
+
+function showPassword2() {
+    const eye = document.getElementById('eye2');
+    const eyeSlash = document.getElementById('eye-slash2');
+    const fielPassword = document.getElementById('fiel-password2');
+
+    if(eye.style.display === 'none') { 
+        eye.style.display = 'block';
+        eyeSlash.style.display = 'none';
+        fielPassword.type = 'text';
+    } else{
+        eye.style.display = 'none';
+        eyeSlash.style.display = 'block';
+        fielPassword.type = 'password';
+    }
+};
+
+/*Esconder senha 2*/ 
+
+function cadastrar(){
+    alert('Botao clicado')
+}
+
+let nome = document.getElementById('#nome')
+let email = document.getElementById('#email')
+let senha = document.getElementById('#fiel-password')
+let confirmsenha = document.getElementById('#fiel-password2')
+
+nome.addEventListener('keyup', ()=> {
+    
+    if(nome.Value.length <= 2){
+        nome.setAttribute('style', 'background-color: greenyellow;')
+
+    } else {
+
+    }
+})
