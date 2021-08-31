022b197694649cff3034c00ee052b92ba78a9b40
Douglas <dougls99@outlook.com>
2021-08-31 08:39:15 -0300

    validação de senha

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+let nome = document.querySelector('#nome')
+let labelnome = document.querySelector('#labelnome')
+let validnome = false
+
+let email = document.querySelector('#email')
+let labelemail = document.querySelector('#labelemail')
+let validemail = false
+
+let senha = document.querySelector('#fiel-password')
+let labelsenha = document.querySelector('#labelsenha')
+let validsenha = false
+
+let confirmsenha = document.querySelector('#fiel-password2')
+let labelconfirmsenha = document.querySelector('#labelconfirmsenha')
+let validconfirmsenha = false
+
 function showPassword() {
     const eye = document.getElementById('eye');
     const eyeSlash = document.getElementById('eye-slash');
@@ -35,20 +51,68 @@ function showPassword2() {
 /*Esconder senha 2*/ 
 
 function cadastrar(){
-    alert('Botao clicado')
+    if(validnome && validemail && validsenha && validconfirmsenha){
+        alert('Deu Certo')
+    }else{
+        alert('algo errado')
+    }
 }
 
-let nome = document.getElementById('#nome')
-let email = document.getElementById('#email')
-let senha = document.getElementById('#fiel-password')
-let confirmsenha = document.getElementById('#fiel-password2')
+/* Botao cadastrar */
 
-nome.addEventListener('keyup', ()=> {
+nome.addEventListener('keyup', () => {
     
-    if(nome.Value.length <= 2){
-        nome.setAttribute('style', 'background-color: greenyellow;')
-
+    if(nome.value.length <= 2 ){
+        nome.setAttribute('style', 'background-color: red;')
+        labelnome.innerHTML='Insira no minimo 3 caracteres'
+        validnome = false
     } else {
+        nome.setAttribute('style', 'background-color: greenyellow;')
+        labelnome.innerHTML='Nome:'
+        validnome = true
+    }
+})
+/* Validaçao do Nome*/
 
+email.addEventListener('keyup', () => {
+    
+    if(email.value.length <= 8 ){
+        email.setAttribute('style', 'background-color: red;')
+        labelemail.innerHTML='Email invalido'
+        let validemail = false
+    } else {
+        email.setAttribute('style', 'background-color: greenyellow;')
+        labelemail.innerHTML='Email:'
+        let validemail = true
+    }
+})
+
+/* Validação do email */
+
+senha.addEventListener('keyup', () => {
+    
+    if(senha.value.length <= 5 ){
+        senha.setAttribute('style', 'background-color: red;')
+        labelsenha.innerHTML='senha invalida'
+        validsenha = false
+       
+    } else {
+        senha.setAttribute('style', 'background-color: greenyellow;')
+        labelsenha.innerHTML='Senha:'
+        validsenha = true
+    }
+})
+/* Validação Senha */
+
+confirmsenha.addEventListener('keyup', () => {
+    
+    if(senha.value != confirmsenha.value){
+        confirmsenha.setAttribute('style', 'background-color: red;')
+        labelconfirmsenha.innerHTML='As Senhas não Conferem'
+        validconfirmsenha = false
+    } else {
+        confirmsenha.setAttribute('style', 'background-color: greenyellow;')
+        labelconfirmsenha.innerHTML='Confirme Senha:'
+        validconfirmsenha = true
     }
 })
